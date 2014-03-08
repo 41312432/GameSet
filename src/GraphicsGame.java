3780996d77f2551dded93d4a9799a22c260bbbcd
jenniferli78 <jenniferli.email@yahoo.com>
2014-03-08 01:56:14 -0500

    worked a little bit on some layout

--- src/GraphicsGame.java
+++ src/GraphicsGame.java
@@ -1,37 +1,66 @@
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
+
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+
 import java.util.ArrayList; 
 
 public class GraphicsGame{
 	private static Dimension PREFERRED_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
     
 	public static void main(String[] args) {
-        final JFrame f = new JFrame("Frame Test");
+        final JFrame f = new JFrame("Graphics Game");
         
-        //create the grid layout
-        //for now the grid takes up the whole window - will need to change
-        int rows=4;
+        //content contains all the panels to be added to JFrame
+        JPanel content = new JPanel(new GridBagLayout()); 
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        //create the grid layout for the cards panel 
+        //Number of cards: can change this to 12 or 15 etc. 
+        //Might need to implement differently, since only need to 3 add cards, 
+        //not replace everything...but for now. 
+        int N=15; 
+        int rows=N/3;
+        
         int cols=3;
         int hgap=3;
         int vgap=3;
         JPanel panel = new JPanel(new GridLayout(rows,cols,hgap,vgap)); 
-        
         //Place N=12 cards from the deck and places them onto panel
         Deck deck = new Deck();
-    	int N=12;
     	placeCards(deck,panel,N);
+        
+    	//creating a filler panel - will replace with submit cards button and such 
+    	JPanel filler = new JPanel();
+    	JLabel plsfill = new JLabel(new ImageIcon("images/01_Purple_Squiggle_Striped.gif"), JLabel.CENTER);
+    	filler.add(plsfill);
     	
-        //JFrame properties 
-        f.setContentPane(panel);
-        //f.setSize(200, 200);
+    	//adding panels to various locations in content panel
+        constraints.insets = new Insets(0, 5, 5, 0);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+    	content.add(panel, constraints);
+    	
+        constraints.insets = new Insets(0, 5, 5, 0);
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+    	content.add(filler,constraints);
+        
+        //Overall JFrame properties 
+        //f.setContentPane(panel);
+        f.getContentPane().add(content);
         f.setSize(PREFERRED_SIZE);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
     }
- 
+	
+	
+	//Methods for the cards panel 
     //Draws N cards and adds their images to the panel 
     //(Cards are not click-able or anything yet)
     public static void placeCards(Deck deck, JPanel panel, int N){
